92318330aaced4340165df076308c372848c6b3f
Hrabal <federicocerchiari@gmail.com>
2018-10-29 20:33:50 +0100

    bugfix

--- cronbat/cronbat.py
+++ cronbat/cronbat.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import os
 import re
 import editor
@@ -35,39 +36,49 @@ class Cron:
             while key_path:
                 k = key_path.pop(0)
                 el.setdefault(k, {})
-                el[k].setdefault('jobs', [])
+                el[k].setdefault('_jobs', [])
                 if not key_path:
-                    el[k]['jobs'].append(Job(cron_str))
+                    el[k]['_jobs'].append(Job(cron_str))
                 el = el[k]
 
     def _yield_crons(self, crondict: dict=None, parents: list=None):
         source = copy(crondict or self.crons)
         parents = parents or []
         for name, section in source.items():
-            yield '# CRONBAT %s CRONBAT' % ':'.join(parents + [name, ]), section
-            if section:
-                parents.append(name)
-                yield from self._yield_crons(section, parents=parents)
-            parents = []
+            if name != '_jobs':
+                yield '# CRONBAT %s CRONBAT' % ':'.join(parents + [name, ]), section
+                if section:
+                    parents.append(name)
+                    yield from self._yield_crons(section, parents=parents)
+                parents = []
 
-    def dump_cron(self):
-        cron_str = '\n\n'.join('%s\n%s' % (
-            name,
-            self._dump_section(cron)
-        ) for name, cron in self._yield_crons())
-        self._write_crontab(cron_str)
+    def dump_cron(self, to_cron: bool=True):
+        if not self.crons:
+            cron_str = ''
+        else:
+            cron_str = '\n\n'.join('%s\n%s' % (
+                name,
+                self._dump_section(cron)
+            ) for name, cron in self._yield_crons())
+        if to_cron:
+            self._write_crontab(cron_str)
+        return cron_str
 
     def _dump_section(self, section: list):
-        return '\n'.join(job.str for job in section.get('jobs', []))
+        return '\n'.join(job.str for job in section.get('_jobs', []))
 
     def edit_section(self, path):
         container = self.crons
         for k in path:
             container = container.get(k, {})
         edit_result = editor.edit(contents=self._dump_section(container).encode('utf-8'))
-        container['jobs'] = []
+        container['_jobs'] = []
         for line in edit_result.decode('utf-8').split('\n'):
             self._add_cron_instruction(path, line)
+
+    def _r(self):
+        self.crons = {}
+        self.dump_cron()
 
 
 class Job:
